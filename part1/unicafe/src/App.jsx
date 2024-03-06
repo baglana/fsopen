b22c6a90499471b5507c81a8ff08a59da7517a7d
baglana <102351104+baglana@users.noreply.github.com>
2024-03-06 08:14:32 +0500

    Do 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,28 +1,37 @@
 import { useState } from 'react'
 
-const Button = ({handleClick, text}) => (
-  <button onClick={handleClick}>{text}</button>
-)
+const Button = ({handleClick, text}) => {
+  return (
+    <button onClick={handleClick}>{text}</button>
+  )
+}
 
-const Display = props => <p>{props.name} {props.value}</p>
+const Display = props => <>{props.name} {props.value}<br /></>
 
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+
+  const setToValue = (newValue, setValue) => {
+    setValue(newValue)
+    setTotal(total + 1)
+  }
 
   return (
     <div>
       <h2>give feedback</h2>
-      <Button handleClick={() => setGood(good + 1)} text={'good'} />
-      <Button handleClick={() => setNeutral(neutral + 1)} text={'neutral'} />
-      <Button handleClick={() => setBad(bad + 1)} text={'bad'} />
+      <Button handleClick={() => setToValue(good + 1, setGood)} text={'good'} />
+      <Button handleClick={() => setToValue(neutral + 1, setNeutral)} text={'neutral'} />
+      <Button handleClick={() => setToValue(bad + 1, setBad)} text={'bad'} />
 
       <h2>statistics</h2>
       <Display name={'good'} value={good} />
       <Display name={'neutral'} value={neutral} />
       <Display name={'bad'} value={bad} />
+      <Display name={'total'} value={total} />
     </div>
   )
 }
