bd509fe6477dd5ab04232517135a873e3a4aa17e
baglana <102351104+baglana@users.noreply.github.com>
2024-03-14 07:47:56 +0500

    Do 2.7

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -12,6 +12,12 @@ const App = () => {
 
   const addPerson = (event) => {
     event.preventDefault();
+
+    if (persons.find(p => p.name === newName)) {
+        alert(`${newName} is already added to phonebook`)
+        return
+    }
+
     setPersons(persons.concat({ name: newName }))
     setNewName('')
   }
