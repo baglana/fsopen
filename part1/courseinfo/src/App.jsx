49131d0200cace398f07740984b7c0e0b51c5758
baglana <102351104+baglana@users.noreply.github.com>
2024-03-14 05:56:05 +0500

    Do 2.1

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,52 +1,56 @@
-const Header = (props) => <h1>{props.course}</h1>;
+const Header = ({ course }) => <h1>{course}</h1>
 
-const Content = (props) => {
+const Total = ({ sum }) => <p>Number of exercises {sum}</p>
+
+const Part = ({ part }) =>
+  <p>
+    {part.name} {part.exercises}
+  </p>
+
+const Content = ({ parts }) =>
+  <>
+    {parts.map(part =>
+      <Part
+        key={part.id} part={part}
+      />
+    )}
+  </>
+
+const Course = (props) => {
   console.log(props)
 
-  return <>
-    <Part name={props.parts[0].name} exercises={props.parts[0].exercises} />
-    <Part name={props.parts[1].name} exercises={props.parts[1].exercises} />
-    <Part name={props.parts[2].name} exercises={props.parts[2].exercises} />
-  </>
-}
-
-const Part = (props) => <p>{props.name} {props.exercises}</p>
-
-const Total = (props) => {
-  const exercises = props.parts.map(part => part.exercises);
-  const total = exercises.reduce((sum, val) => sum + val);
-
   return (
-    <p>Number of exercises {total}</p>
+    <>
+      <Header course={props.course.name} />
+      <Content parts={props.course.parts} />
+    </>
   )
 }
 
 const App = () => {
   const course = {
+    id: 1,
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
-      }
+        exercises: 14,
+        id: 3
+      },
     ]
   }
 
-  return (
-    <div>
-      <Header course={course.name}/>
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
+  return <Course course={course} />
 }
 
 export default App
