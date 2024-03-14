74acfab5a579611cd8b680edcb535ec98f04288a
baglana <102351104+baglana@users.noreply.github.com>
2024-03-14 06:14:01 +0500

    Do 2.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,36 +1,4 @@
-const Header = ({ course }) => <h1>{course}</h1>
-
-const Total = ({ sum }) => <b>total of {sum} exercises</b>
-
-const Part = ({ part }) =>
-  <p>
-    {part.name} {part.exercises}
-  </p>
-
-const Content = ({ parts }) =>
-  <>
-    {parts.map(part =>
-      <Part
-        key={part.id} part={part}
-      />
-    )}
-  </>
-
-const Course = (props) => {
-  console.log(props)
-
-  const { name, parts } = props.course;
-
-  const total = parts.reduce((s, p) => s + p.exercises, 0)
-
-  return (
-    <>
-      <Header course={name} />
-      <Content parts={parts} />
-      <Total sum={total} />
-    </>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
